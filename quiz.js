a86040a3d9f534f22275e698b955b9ab18bcdb36
monylangmading-ops <monylangmading@gmail.com>
2025-10-03 12:37:50 -0700

    Update stuff

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkanswer(){
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-if (userAnswer = correctAnswer) {
+if (userAnswer === correctAnswer) {
    feedback.textContent = "Correct! Well done.";
 } else {
    feedback.textContent = "That's incorrect. Try again!";
