ab8fb3407d8c7c07827669fe5ee551f0af2dad15
monylangmading-ops <monylangmading@gmail.com>
2025-10-03 12:31:00 -0700

    Update stuff

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkanswer(){
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+if (userAnswer = correctAnswer) {
+   feedback.textContent = "Correct! Well done.";
+} else {
+   feedback.textContent = "That's incorrect. Try again!";
+}
+const feedback = document.getElementById('feedback');
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+
+};
